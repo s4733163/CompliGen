c9b55c6d77f6532ade56ca1a853b6b6881e985a9
s4733163 <141945058+s4733163@users.noreply.github.com>
2026-02-19 16:41:59 +0400

    Added the comments.

--- CompliGen/policy_generator/urls.py
+++ CompliGen/policy_generator/urls.py
@@ -3,12 +3,14 @@ from .views import *
 
 # all the urls for generating the 5 docs
 urlpatterns = [
+    # generating policies and retrieving all
     path('api/tos', TermsOfServiceView.as_view(), name='ToS'),
     path('api/privacypolicy', PrivacyPolicyView.as_view(), name='Privacy'),
     path('api/cookie', CookiePolicyView.as_view(), name='Cookie'),
     path('api/dpa',  DataProcessisingAgreementView.as_view(), name='dpa'),
     path('api/aup', AcceptableUsePolicyView.as_view(), name='aup'),
 
+    # basically deleting the policies
     path('api/tos/<int:id>', TermsOfServiceDeleteView.as_view(), name='tos-delete'),
     path('api/privacypolicy/<int:id>', PrivacyPolicyDeleteView.as_view(), name='privacy-delete'),
     path('api/cookie/<int:id>', CookiePolicyDeleteView.as_view(), name='cookie-delete'),
